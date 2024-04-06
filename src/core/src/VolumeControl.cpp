37e501fb34839aace0ec0f5a893f5eef9f301d24
Fábio Pichler <fabiopichler1992@gmail.com>
2024-04-06 03:08:19 -0300

    updated

--- src/core/src/VolumeControl.cpp
+++ src/core/src/VolumeControl.cpp
@@ -51,6 +51,7 @@ VolumeControl::VolumeControl(QWidget *parent)
 
     m_button = new QPushButton(popup);
     m_button->setObjectName("volumeButton");
+    m_button->setProperty("class", "iconButton iconButton2");
     m_button->setToolTip("Desativar som");
 
     m_slider = new Slider(popup);
